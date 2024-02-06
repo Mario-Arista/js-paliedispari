dc4b8e2feb353ce6089a553032980fae6a4c6b82
mario.arista <mario.arista.dev@gmail.com>
2024-02-06 16:56:32 +0100

    pari e dispari works (with console log) But sum done without function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
-// PARI O DISPARI
-
-/* 
-L’utente sceglie pari o dispari.
-Inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) 
-per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è 
-pari o dispari (usando una funzione)
-Se l'utente ha indovinato l'esito 
-(pari o dispari) della somma ha vinto, 
-altrimenti ha perso.
-*/
 // Mi salvo Button
 const buttonElement = document.getElementById("button-pd");
-
-// Mi salvo Input Utente
-const sceltaUtenteValore = (document.getElementById("numero-utente-pd").value);
-const sceltaNumeroValore = Number(document.getElementById("scelta-utente-pd").value);
-
 
 // Genero Numero Random Computer con function
 
@@ -28,32 +9,28 @@ function computerButtaNumero() {
     return numeroComputer;
 }
 
-const numeroRandom = computerButtaNumero();
-console.log(numeroRandom);
-
-// Genero se la somma dei due numeri è pari/dispari
-
-const numeroPari = true;
-
-function checksomma() {
-
-    const somma = computerButtaNumero() + sceltaNumeroValore;
-
-    if (somma % 2 != 0) {
-        numeroPari = false;
-    } else {
-        return numeroPari;
-    }
-
-}
-
+console.log(computerButtaNumero());
 
 // Evento al click
 
 buttonElement.addEventListener("click", 
     function() {
 
+        // Input Utente
+        const sceltaPariDispariUtente = (document.getElementById("scelta-utente-pd").value);
+        const sceltaNumeroUtente = Number(document.getElementById("numero-utente-pd").value);
 
+        // NUMERO RANDOM COMPUTER
+        const numeroRandom = computerButtaNumero();
+
+        // faccio somma dei 2 numeri
+        const somma = numeroRandom + sceltaNumeroUtente;
+
+        if ((sceltaPariDispariUtente === "pari" && somma % 2 == 0 ) || (sceltaPariDispariUtente === "dispari" && somma % 2 != 0))   {
+            console.log("Hai vinto");
+        } else {
+            console.log("Hai perso");
+        }
     }
 );
 
